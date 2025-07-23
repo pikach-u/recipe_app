1d5565947781a52a0eb1c9e5af7d6317b57475aa
pikach-u <ziwon05@naver.com>
2025-07-24 02:02:17 +0900

    Recipe 조회 & 검색 통합으로 구현 완료 Dynamic Query

--- src/main/java/com/pikachu/recipe_app/dto/RecipeResponseDto.java
+++ src/main/java/com/pikachu/recipe_app/dto/RecipeResponseDto.java
@@ -1,9 +1,14 @@
 package com.pikachu.recipe_app.dto;
 
+import com.pikachu.recipe_app.model.Ingredient;
+import com.pikachu.recipe_app.model.Recipe;
+import com.pikachu.recipe_app.model.RecipeIngredient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +18,13 @@ public class RecipeResponseDto {
     private Long id;
     private String title;
     private String description;
+    private List<RecipeIngredient> recipeIngredientList;
+
+    //엔티티를 그대로 반환하지 않고 응답용DTO에 Recipe를 인자로 가지는 생성자 추가 (검색용)
+    public RecipeResponseDto(Recipe recipe) {
+        this.id = recipe.getId();
+        this.title = recipe.getTitle();
+        this.description = recipe.getDescription();
+        this.recipeIngredientList = recipe.getRecipeIngredients();
+    }
 }
--- src/main/java/com/pikachu/recipe_app/repository/RecipeRepository.java
+++ src/main/java/com/pikachu/recipe_app/repository/RecipeRepository.java
@@ -7,6 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long>, JpaSpecificationExecutor<Recipe> {
-    // 고민
-    Page<Recipe> findByTitleContaining(String title, Pageable pageable);
+
 }
