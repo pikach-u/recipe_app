fda424482626b724dbce676ce1f170ab99cd809b
pikach-u <ziwon05@naver.com>
2025-07-25 23:55:04 +0900

    레시피 ThymeLeaf View 구현 중 재료 관리 페이지 오류 수정

--- src/main/java/com/pikachu/recipe_app/controller/IngredientViewController.java
+++ src/main/java/com/pikachu/recipe_app/controller/IngredientViewController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/ingredient")
+@RequestMapping("/ingredients")
 public class IngredientViewController {
     private final IngredientService ingredientService;
 
